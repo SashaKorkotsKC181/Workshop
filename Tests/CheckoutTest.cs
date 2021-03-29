452d2ba29a5202128b93a94b72563928c212da36
Olexander Korkots <k.sasha.g@gmail.com>
2021-03-29 13:45:10 +0300

    03 check close and auto open test

--- Tests/CheckoutTest.cs
+++ Tests/CheckoutTest.cs
@@ -7,27 +7,41 @@ namespace Tests
     public class ChekoutTesk
     {
         [Fact]
-        public void closeCheck_withAddProduct()
+        void closeCheck_withAddProduct()
         {
             CheckoutServise checkoutServise = new CheckoutServise();
             checkoutServise.openCheck();
 
-            checkoutServise.AddProduct(new Product(7,"Milk"));
+            checkoutServise.AddProduct(new Product(7, "Milk"));
             Check check = checkoutServise.closeCheck();
             Assert.Equal(check.GetTotalCost(), 7);
         }
 
         [Fact]
-        public void closeCheck_withAddTwoProducts()
+        void closeCheck_withAddTwoProducts()
         {
             CheckoutServise checkoutServise = new CheckoutServise();
             checkoutServise.openCheck();
 
-            checkoutServise.AddProduct(new Product(7,"Milk"));
-            checkoutServise.AddProduct(new Product(3,"Bread"));
+            checkoutServise.AddProduct(new Product(7, "Milk"));
+            checkoutServise.AddProduct(new Product(3, "Bread"));
             Check check = checkoutServise.closeCheck();
 
             Assert.Equal(check.GetTotalCost(), 10);
+        }
+
+        [Fact]
+        void AddProduct_whenCHeckIsCLosed_opensNewCheck()
+        {
+            CheckoutServise checkoutServise = new CheckoutServise();
+
+            checkoutServise.AddProduct(new Product(7, "Milk"));            
+            Check checkMilk = checkoutServise.closeCheck();
+            Assert.Equal(checkMilk.GetTotalCost(), 7);
+
+            checkoutServise.AddProduct(new Product(3, "Bread"));
+            Check checkBread = checkoutServise.closeCheck();
+            Assert.Equal(checkBread.GetTotalCost(), 3);
         }
     }
 }
